cc6c2c9cedd67fc93c0273f5170227fbe40aa0e1
Tiffany Du <tadofdesign@gmail.com>
2017-03-21 13:51:07 -0500

    Implemented Unsplash API for random BG image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // AJAX Post HTTP Request
+  // Request random quote from quotesondesign.com
   function getRandomQuote() {
     var quoteRequest = $.ajax({
       type: 'GET',
@@ -8,20 +8,49 @@ $(document).ready(function () {
       cache: false
     });
     quoteRequest.done(function (data) {
-      var quote = data[0].content;
+      // Remove <p> tags and trailing white space
+      var quote = data[0].content.slice(3,-5).trim();
       var author = data[0].title;
-      var link = data[0].link;
-      $('#quoteContent').html(quote);
+      $('#quoteContent').html('&ldquo; ' + quote + ' &rdquo;');
       $('#quoteAuthor').html(author);
-      $('#quoteSource').html(link);
-      console.log(data[0]);
     });
     quoteRequest.fail(function (xhr, status, error) {
       console.warn(xhr.responseText);
     });
   }
-  $('button#getQuote').on('click', function (e) {
+
+  // Request random photo from Unsplash
+  function getRandomBackground() {
+    var photoRequest = $.ajax({
+      type: 'GET',
+      url: 'https://api.unsplash.com/photos/random?client_id=9aa2f67b98d0fc60047248749538fb79be5414742a022301205a0817af275b73&query=design',
+      datatype: 'json',
+      cache: false
+    });
+    photoRequest.done(function (data) {
+      var photoURL = data.urls.regular;
+      var photoUser = data.user.name;
+      var photoUserURL = data.user.links.html;
+      $('body').css('background-image', 'url(' + photoURL + ')');
+      $('#photoCredit').html(photoUser).attr({
+        href: photoUserURL,
+        alt: photoUser,
+        title: photoUser
+      });
+    });
+    photoRequest.fail(function (xhr, status, error) {
+      console.warn(xhr.responseText);
+    });
+  }
+
+  // Get another random quote and background image on click
+  $('#getQuoteBtn').on('click', function (e) {
     e.preventDefault();
     getRandomQuote();
+    getRandomBackground();
   });
+
+  // Get random quote and background image on load
+  getRandomQuote();
+  getRandomBackground();
 });
